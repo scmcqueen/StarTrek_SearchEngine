3ce5b93223be373a9d8031ddae2baeb1b3c35ce4
Skyeler McQueen <scm237@cornell.edu>
2025-02-23 20:16:39 -0500

    load search engine in panel

--- panel.py
+++ panel.py
@@ -23,8 +23,18 @@ pn.extension(design="material", sizing_mode="stretch_width")
 
 # data.tail()
 
-# Functions
+# LOAD EVERYTHING
+complete = pd.read_csv('../StarTrekNextGenScriptData/complete_data.csv')
+complete.columns = ['index', 'character', 'quote', 'scene', 'location', 'view',
+       'episode', 'date', 'series', 'file']
+test_engine = ss.search_engine()
+test_engine.bulk_load(complete[['quote']].to_dict()['quote'])
 
+# functions
+def search(search_engine:ss.search_engine,query:str,num_results:int=20)->list:
+    '''Completes a search of a term on an engine'''
+    results=search_engine.bw_search(query,num_results)
+    return(search_engine.pretty_print(results))
 
 # Set Widget
 text_input = pn.widgets.TextInput(name='Text Input', placeholder='Enter a string here...')
