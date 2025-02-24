99d05002341b81930c6047bf2edc494cc6452f2f
Skyeler McQueen <scm237@cornell.edu>
2025-02-23 20:07:14 -0500

    cleaning up example code

--- panel.py
+++ panel.py
@@ -21,23 +21,7 @@ data = get_data()
 data.tail()
 
 # Functions
-def transform_data(variable, window, sigma):
-    """Calculates the rolling average and identifies outliers"""
-    avg = data[variable].rolling(window=window).mean()
-    residual = data[variable] - avg
-    std = residual.rolling(window=window).std()
-    outliers = np.abs(residual) > std * sigma
-    return avg, avg[outliers]
 
-
-def get_plot(variable="Temperature", window=30, sigma=10):
-    """Plots the rolling average and the outliers"""
-    avg, highlight = transform_data(variable, window, sigma)
-    return avg.hvplot(
-        height=300, legend=False, color=PRIMARY_COLOR
-    ) * highlight.hvplot.scatter(color=SECONDARY_COLOR, padding=0.1, legend=False)
-
-get_plot(variable='Temperature', window=20, sigma=10)
 
 # Set Widget
 text_input = pn.widgets.TextInput(name='Text Input', placeholder='Enter a string here...')
