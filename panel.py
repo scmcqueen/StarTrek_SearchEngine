8d4aea8c3e1eaf1892e78b27086fa81407980039
Skyeler McQueen <scm237@cornell.edu>
2025-04-14 17:25:37 -0400

    actually make an interface for my search engine

--- panel.py
+++ panel.py
@@ -26,6 +26,10 @@ import preprocess_functions as pp # gets the data preprocessing function
 grey = '#eaeaea'
 green = '#83f1b2'
 
+# load pipeline & model
+pipeline = joblib.load('data_preprocessing_pipeline.pkl') 
+model = lgb.Booster(model_file='ranker_041425.txt')
+
 
 pn.extension( global_css=[''':root { --design-primary-color: black;
     --mdc-theme-background: XXX; /* Grey background */}
@@ -42,19 +46,23 @@ pn.extension(raw_css=['''.custom-button {
     }
     '''.replace('YYY',green)])
 
+# fucntions
 
 
-# Start by loading in the data
-complete = pd.read_csv('https://scmcqueen.github.io/StarTrekScriptData/complete_data.csv')
-# Rename columns
-complete.columns = ['index', 'character', 'quote', 'scene', 'location', 'view',
-       'episode', 'date', 'series', 'file']
-# Clean up Character & Quote
-complete['character'] = complete['character'].apply(lambda text: " ".join(str(text).split()))
-complete['quote']=complete['quote'].apply(lambda text: " ".join(text.split()))
-# test_engine = ss.search_engine()
-# test_engine.bulk_load(complete[['quote']].to_dict()['quote'])
-# test_engine.add_df(complete)
+# read in the complete data
+complete = pd.read_csv('complete_sentiment.csv',index_col=0)
+# drop unneeded columns
+complete.drop(columns=['Unnamed: 0.1','Unnamed: 0'],inplace=True)
+# fill na characters
+complete['character'] = complete['character'].fillna('NA')
+
+# read in sentence embeddings
+embed_df = pd.read_csv('st_embeddings.csv',index_col=0)
+# rename columns
+embed_df.columns = [f'embedding_{x}' for x in embed_df.columns]
+# get an index col for merging
+embed_df=embed_df.reset_index()
+
 
 # instantiate the search engine & load data
 search_engine = se.search_engine(name='BM25 Engine',full_data=complete)
@@ -62,53 +70,66 @@ search_engine.bulk_load(complete[['quote']].to_dict()['quote'])
 # to query, we need to do search_engine.bw_search(term,20,context=True)
 
 # globals
-ranking = []
+# ranking = []
 results = None
-craines = pn.widgets.Button(name='Craines')
+search_init = False
+# craines = pn.widgets.Button(name='Craines')
 
 # functions
 # search_results = []
-# def search(search_engine:ss.search_engine,query:str,num_results:int=30)->list:
-#     global search_results, results
-#     '''Completes a search of a term on an engine'''
-#     results=search_engine.bw_search(query,num_results)
-#     search_results = search_engine.pretty_print([x[0] for x in results])
-#     pn.main = [search_results] # tried this
-#     print('success')
-#     return(search_engine.pretty_print([x[0] for x in results]))
+def search(keyword:str)->list:
+    global search_engine, embed_df, complete, pipeline, model, results, search_init
+    search_init = True
+    copy_complete = complete.copy()
+    copy_embed = embed_df.copy()
+    # start by setting the column
+    # now start search
+    q_results = search_engine.bw_search(keyword,20,context=True)
+    results_df = pd.DataFrame(q_results)
+    results_df.columns = ['index','bm25','prevbm25','nextbm25']
+    print('We have 20 results')
+    # start with merge data
+    print('merging')
+    merged = pp.merge_data(results_df,copy_complete,copy_embed)
 
-def pretty_print(event):
-    global search_results, ranking, craines
-    temp_open = pn.pane.Markdown('### Results')
-    column = pn.Column(temp_open)
-    # output =  ''
-    # counter = 0
-    # ranking = []
-    # for x in search_results:
-    #     output+='"'+re.sub(r'\s+', ' ', x[0]).strip()+'"'
-    #     output+='\n'
-    #     output+='**'
-    #     output+=x[1]
-    #     # output+='"'+re.sub(r'\s+', ' ', x[1]).strip()+'"'
-    #     output+='**'
-    #     output+='\n'
-    #     output+='"'+re.sub(r'\s+', ' ', x[2]).strip()+'"'
-    #     column.append(pn.pane.Markdown(output))
-    #     output=''
-    #     # temp = pn.widgets.TextInput(name=f'Ranking {str(counter)}', placeholder='candle')
-    #     radio = pn.widgets.RadioButtonGroup(name=f'Ranking {str(counter)}', 
-    #                                         options=['Relevant','No'], button_type='success')
-    #     ranking.append(radio)
-    #     column.append(radio)
-    #     counter +=1
-    # # TEMPORARILY COMMENT EVERYTHING OUT
-    column.append(pn.pane.Markdown('''
-        RIKER: I don't like fudge.
-        **TROI: Really. I never met a chocolate I didn't like.**
-        RIKER: Doesn't it taste good?
+    # get the feature engineering
+    print('engineering')
+    engineered = pp.feature_engineering(merged,copy_complete)
+    # for x in engineered.columns:
+    #     if 'embed' not in str(x):
+    #         print(x)
+    # print('\n')
+    print('transforming')
+    transformed = pipeline.transform(engineered)
+    # now I can make predictions!
+    print('predicting')
+    preds = model.predict(transformed)
+    # add to data
+    results_df['preds']=preds
+    # rank
+    results_df['rank']=results_df['preds'].rank(method='max')
+    # sort
+    results = results_df.sort_values('rank')
+    print('finished searching')
+    return results
 
-        _Star Trek: The Next Generation_, “The Game”
-    '''))
+def pretty_print(event): # event
+    # global search_results, ranking, craines
+    # temp_open = pn.pane.Markdown('### Results')
+    # column = pn.Column(temp_open)
+    global results, search_init
+    print('starting printing')
+    opening = pn.pane.Markdown('### Results')
+    column = pn.Column(opening)
+    print(search_init)
+    if not search_init:
+        print('no data')
+        return(column)
+    for ind in results['index']:
+        # get the pretty printed result
+        pretty = search_engine.pretty_print(ind)
+        column.append(pn.pane.Markdown(pretty))
+    print('done printing')
     return(column)
 
 
@@ -118,20 +139,19 @@ text_input = pn.widgets.TextInput( placeholder='Try "candle"',width = 550)
 search_button = pn.widgets.Button(name='Search',css_classes=['custom-button'],
     button_type='light',button_style='outline', align="center")
 # bind
-search_button.on_click(lambda event: search(test_engine,text_input.value)) # lambda x
+search_button.on_click(lambda event: search(text_input.value)) # lambda x
 
 title_text = pn.pane.Markdown('## A _Star Trek_ Search Engine that prioritizes humor', align="center")
-
 # Serve
 pn.template.MaterialTemplate(
     title="Ad Aspera per Data",
     main=pn.Column(title_text,
     pn.Row(#pn.Spacer(sizing_mode="stretch_width"),
         text_input,
-        search_button,
-        #pn.Spacer(sizing_mode="stretch_width")
+        search_button
         ),
-    pn.bind(pretty_print,event=search_button)),
+    #pn.bind(pretty_print,search_button)),
+    pn.bind(pretty_print,search_button.param.clicks))
 ).servable() # The ; is needed in the notebook to not display the template. Its not needed in a script
 
 # should add character names to results
